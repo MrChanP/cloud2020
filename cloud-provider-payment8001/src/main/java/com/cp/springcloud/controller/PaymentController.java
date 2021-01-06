87f4472cf7fb905b2f89448801f1052dbee027f2
chanp <chanpwork@163.com>
2021-01-06 23:28:15 +0800

    20200106

--- cloud-provider-payment8001/src/main/java/com/cp/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/cp/springcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.cp.springcloud.entities.CommontResult;
 import com.cp.springcloud.entities.Payment;
 import com.cp.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -19,7 +16,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommontResult create(Payment payment){
+    public CommontResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("*****插入结果：" + result);
         if (result > 0) {
